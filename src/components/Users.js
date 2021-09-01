4b7b7b92dadbb0dcb3dabc892e33d752abcc9fc2
kankit08 <kankit08@gmail.com>
2021-09-01 18:39:31 +0530

    Issue resolved

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { USER_PER_PAGE } from '../utils/constants';
 import User from './User';
-import {uuid} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 const Users = ({users, page}) => {
     const startIndex = ( page - 1 ) * USER_PER_PAGE;
     const selectedUsers = users.slice(startIndex, startIndex + USER_PER_PAGE);
     return selectedUsers.map(user => (
-        <User user={user} key={user.uuid}/>
+        <User user={user} key={uuidv4()}/>
     ))
 }
 
